7a503c8eca794202c1556e50abd0933add02453f
luucdamaso <107771309+luucdamaso@users.noreply.github.com>
2022-08-23 19:42:35 -0300

    Exercício

--- aula03/index.js
+++ aula03/index.js
@@ -3,3 +3,4 @@ console.log('Lucas Damaso'); // Number = Número
 console.log(`'Lucas'"Damaso"`);
 
 console.log(35, 15.85, 'Lucas Damaso');
+console.log(`Meu nome é "Lucas". Estou aprendendo JavaScript às`, 19,`da noite`)
