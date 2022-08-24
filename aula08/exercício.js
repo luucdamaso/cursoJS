fab76bedcaf178a553695af056053e07d98a8431
luucdamaso <107771309+luucdamaso@users.noreply.github.com>
2022-08-24 12:54:04 -0300

    aula08 exercícios de const e let

---
+++ aula08/exercício.js
@@ -0,0 +1,8 @@
+const nome = 'Lucas';
+const sobrenome = 'Damaso';
+const idade = 25;
+const peso = 76; 
+const altura = 1.83;
+let imc = (peso / (altura*altura))
+let nascimento = (2022-idade)
+console.log(`Boa tarde Sr(a) ${nome} ${sobrenome} como o Sr(a) informou que tem ${idade} anos então quer dizer que nasceu no ano de ${nascimento}, e com base no seu peso de ${peso} kg e altura de ${altura} cm o seu imc é: ${imc}`)
