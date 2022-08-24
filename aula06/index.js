8fcd7b1576e4a6f76b6466aae26b4f265770e4fc
luucdamaso <107771309+luucdamaso@users.noreply.github.com>
2022-08-24 11:48:31 -0300

    aula 06

---
+++ aula06/index.js
@@ -0,0 +1,16 @@
+let nome = 'João'; //String
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000',nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com' ,nome, 'em 2015.');
+console.log('O filho de' ,nome, 'se chama Eduardo');
+
+//Não podemos criar variaveis com palavras reservadas
+//Variáveis precisam ter nomes significativos
+//Não podemos começar o nome de uma variável com um número
+//não podem conter espaços ou traços
+//Ultilizamos camelCase
+//Case-sensitive
+//Não podemos redeclarar variável com Let
+//NÃO ULTILIZE VAR, UTILIZE LET.
