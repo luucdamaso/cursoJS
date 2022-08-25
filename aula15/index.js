45ff77c9cb01e19139eedc316dd7eb8f498dacff
luucdamaso <107771309+luucdamaso@users.noreply.github.com>
2022-08-25 12:04:46 -0300

    Aula15 Objeto Math

---
+++ aula15/index.js
@@ -0,0 +1,9 @@
+let num1 = 25;
+console.log(num1 ** 0.5)
+
+/* let num2 = Math.floor(num1); //Arredonda meu número para baixo
+let num2 = Math.ceil(num1);  //Arredonda meu número para cima 
+let num2 = Math.round(num1); // Arredondamento para cima ou pra baixo 
+console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6)); // Pega o maior número de uma lista de números
+console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6)); Pega o menor número de uma lista de números
+const aleatorio = Math.round(Math.random() * (10-5) + 5); //Vai pegar sempre um número maior que 5 e menor que 10 e arredondar. */
