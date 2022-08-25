0b123febee9b1378d5c3fd41e3ab1e773cc890a1
luucdamaso <107771309+luucdamaso@users.noreply.github.com>
2022-08-25 16:50:33 -0300

    aula17 Funções (Básico)

---
+++ aula17/index.js
@@ -0,0 +1,26 @@
+/* FUNÇÃO TRADICIONAL
+function saudacao(nome) {
+    console.log(`Bom dia ${nome}!`);
+}
+
+saudacao('Luiz');
+saudacao('Maria');
+saudacao('Felipe');
+
+____________________________________________________
+
+function soma(x, y) {
+    const resultado = x + y;
+    return resultado;
+}
+
+const resultado = soma();
+console.log(resultado)*/
+
+/* NOVO TIPO DE FUNÇÃO
+const raiz = n => n ** 0.5; 
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
+*/
