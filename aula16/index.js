41cd242e3d4c42d12fca0e4d18418bfd050949cd
luucdamaso <107771309+luucdamaso@users.noreply.github.com>
2022-08-25 15:55:24 -0300

    aula16 Arrays (Básico)

---
+++ aula16/index.js
@@ -0,0 +1,23 @@
+const alunos = ['Luiz', 'Maria', 'João'];
+
+console.log(alunos instanceof Array);
+
+/*  PEDIR PARA MOSTRAR ITENS ESPECIFICOS DA LISTA
+console.log(alunos.slice(0, 1));  
+console.log(alunos.slice(0, -1));  
+*/
+
+/*
+delete alunos[1]; //Deleta o valor do índice mas a marcação não some do índice
+
+const removido = alunos.shift(); //Remove o último na lista do Array
+const removido = alunos.pop(); //Remove o primeiro na lista do Array
+
+alunos.unshift('Fábio'); //Adiciona no começo
+alunos.push('Luiza'); //Adciona no fim
+
+alunos [alunos.lenght] = 'Luiza'; //Adiciona no fim 
+
+alunos [0] = 'Eduardo'; //Altera
+alunos [3] = 'Luiza' ;
+*/
